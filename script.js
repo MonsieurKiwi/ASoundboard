858767785536f697b1a43ff887c034c20d104b10
Monsieur Kiwi <73957517+MonsieurKiwi@users.noreply.github.com>
2024-01-26 22:23:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const sounds = [
+	'airhorn',
+	'applause',
+	'aww',
+	'badumtss',
+	'blueorred',
+	'cartooncarhorn',
+	'cartoondeflate',
+	'cartoonspring',
+	'cashregister',
+	'clockticking',
+	'crickets',
+	'cuckoo',
+	'dundundunn',
+	'fightclub',
+	'groove',
+	'houston',
+	'illbeback',
+	'investigating',
+	'kidscheering',
+	'partyhorn',
+	'recordscratch',
+	'smallstepforman',
+	'wahwahwah',
+	'wrong',
+	'rubberduck',
+	'yehah',
+	'serious',
+	'stop',
+];
+
+sounds.forEach((sound) => {
+	const btn = document.createElement('button');
+	btn.classList.add('btn');
+
+	btn.innerText = sound;
+
+	btn.addEventListener('click', () => {
+		stopSongs();
+		document.getElementById(sound).play();
+	});
+
+	document.getElementById('buttons').appendChild(btn);
+});
+
+function stopSongs() {
+	sounds.forEach((sound) => {
+		const song = document.getElementById(sound);
+		song.pause();
+		song.currentTime = 0;
+	});
+}
